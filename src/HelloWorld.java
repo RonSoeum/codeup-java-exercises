917ce8878402fb2e1e7cb56eb4609a088ee2d633
Ron Soeum <ronsoeum@gmail.com>
2019-01-07 16:24:36 -0600

    adjusted answers

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -22,8 +22,8 @@ public class HelloWorld {
 //        System.out.println(myString);
 
 //        4. Change your code to assign the value 3.14159 to myString. What happens?
-        myString = "3.14159";
-        System.out.println(myString);
+//        myString = 3.14159;
+//        System.out.println(myString);
 
 //        5. Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
 //        long myNumber;
@@ -41,10 +41,10 @@ public class HelloWorld {
 //        long myNumber = 123;
 //        System.out.println(myNumber);
 
-
 //        9. Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
 //        float myNumber = 3.14;
-        float myNumber = 3.14F;
+//        float myNumber = 3.14F;
+        double myNumber = 3.14;
         System.out.println(myNumber);
 
 //        10. Copy and paste the following code blocks one at a time and execute them.
@@ -57,7 +57,7 @@ public class HelloWorld {
 //        System.out.println(x);
 
 //        11. Try to create a variable named class. What happens?
-//        var class;
+//        String class;
 
 //        12. Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
 //        String theNumberThree = "three";
