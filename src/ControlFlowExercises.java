f595b742b152a7d723ce9ffe0a03e3cb8c483f25
Ron Soeum <ronsoeum@gmail.com>
2019-01-09 09:01:08 -0600

    4

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -109,10 +109,36 @@ public class ControlFlowExercises {
 
                 }while(userConfirm.equalsIgnoreCase("yes") || userConfirm.equalsIgnoreCase("y"));
 
+//        4.Convert given number grades into letter grades.
 
+//            Prompt the user for a numerical grade from 0 to 100.
+//            Display the corresponding letter grade.
+//            Prompt the user to continue.
+//            Assume that the user will enter valid integers for the grades.
+//            The application should only continue if the user agrees to.
+//            Grade Ranges:
+//
+//                A : 100 - 88
+//                B : 87 - 80
+//                C : 79 - 67
+//                D : 66 - 60
+//                F : 59 - 0
 
-
-
+                System.out.println("Enter a numerical grade from 0 to 100.");
+                int numericGrade = scanner.nextInt();
+                String letterGrade;
+                if(numericGrade >= 88) {
+                    letterGrade = "A";
+                } else if(numericGrade >= 80) {
+                    letterGrade = "B";
+                } else if(numericGrade >= 67) {
+                    letterGrade = "C";
+                } else if(numericGrade >= 60) {
+                    letterGrade = "D";
+                } else {
+                    letterGrade = "F";
+                }
+                System.out.println("Grade is a " + letterGrade);
 
     }
 
