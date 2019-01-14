5bf12d6a8978263ea9659309b41b3455e7f7b08a
Ron Soeum <ronsoeum@gmail.com>
2019-01-14 13:22:03 -0600

    Exercises - 3

---
+++ src/util/Input.java
@@ -0,0 +1,68 @@
+package util;
+import java.util.Scanner;
+
+public class Input {
+
+    private static String getString(){
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        return scan.next();
+    }
+
+    public static boolean yesNo(){
+        System.out.println("Do you want to continue?");
+        String input = getString();
+        if(input.equalsIgnoreCase("yes")
+                || input.equalsIgnoreCase("y")
+                || input.equalsIgnoreCase("no")
+                || input.equalsIgnoreCase("n")){
+            if(input.equalsIgnoreCase("yes")
+                    || input.equalsIgnoreCase("y")){
+                System.out.println("true");
+                return true;
+            }else{
+                System.out.println("false");
+                return false;
+            }
+        }else{
+            System.out.println("Enter \"yes\" or \"no\" :");
+            return yesNo();
+        }
+    }
+
+    //==============================================================================
+
+    private static int getInt(){
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        return scanner.nextInt();
+    }
+
+    public static int getInt(int min, int max){
+        System.out.printf("Enter an integer number between %d and %d:%n", min, max);
+        int input = getInt();
+        if(input >= min & input <= max){
+            System.out.printf("Your number %d is within the range.%n", input);
+            return input;
+        }else{
+            return getInt(min, max);
+        }
+    }
+
+    //==============================================================================
+
+    private static double getDouble(){
+        Scanner scanner = new Scanner(System.in).useDelimiter("\n");
+        return scanner.nextDouble();
+    }
+
+    public static double getDouble(double min, double max){
+        System.out.printf("Enter a decimal number between %.2f and %.2f:%n", min, max);
+        double input = getDouble();
+        if(input >= min & input <= max){
+            System.out.printf("Your number %.2f is within the range.%n", input);
+            return input;
+        }else{
+            return getDouble(min, max);
+        }
+    }
+
+}//class
---
+++ src/util/InputTest.java
@@ -0,0 +1,15 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args) {
+
+//        Input.yesNo();
+//
+//        Input.getInt(12, 345);
+
+        Input.getDouble(112.036, 250.05);
+
+    }
+
+}
