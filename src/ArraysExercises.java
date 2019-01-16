5e4b45663f34ff10d69275c91c58508ec0dec8d1
Ron Soeum <ronsoeum@gmail.com>
2019-01-16 09:11:18 -0600

    refactor code to be more concise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -8,25 +8,17 @@ public class ArraysExercises {
         System.out.println(numbers);// reference memory location
         System.out.println(Arrays.toString(numbers));
 
-
-        Person person1 = new Person("John");
-        Person person2 = new Person("Ron");
-        Person person3 = new Person("Eric");
-
         Person[] people1 = new Person[3];
-        people1[0] = person1;
-        people1[1] = person2;
-        people1[2] = person3;
+        people1[0] = new Person("John");
+        people1[1] = new Person("Ron");
+        people1[2] = new Person("Eric");
 
         for(Person person: people1){
             person.getName();
             System.out.println("===============\n");
         }
 
-
-
-        Person person4 = new Person("Sam");
-        Person[] people2 = addPerson(people1, person4);// returns the new array, how to access it?
+        Person[] people2 = addPerson(people1, new Person("Sam"));
 
         for(Person person: people2){
             person.getName();
