cf1afbed11856cf2c2453e332bdf45aedb04d448
Ron Soeum <ronsoeum@gmail.com>
2019-01-16 11:10:49 -0600

    aded option to add a new movie to list

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,5 +1,8 @@
 package movies;
 import util.Input;
+
+import java.util.Arrays;
+
 import static movies.MoviesArray.findAll;
 
 public class MoviesApplication {
@@ -20,11 +23,12 @@ public class MoviesApplication {
         System.out.println("2 - view movies in the animated category");
         System.out.println("3 - view movies in the drama category");
         System.out.println("4 - view movies in the horror category");
-        System.out.println("5 - view movies in the sci-fi category\n");
+        System.out.println("5 - view movies in the sci-fi category");
+        System.out.println("6 - add a new movie to the list\n");
     }// userOptions
 
     public static void userChoice(){
-        int userInput = input.getInt(0, 5, "Enter your choice");
+        int userInput = input.getInt(0, 6, "Enter your choice");
         if(userInput == 0){
             System.exit(0);// 0
         }
@@ -53,13 +57,20 @@ public class MoviesApplication {
             System.out.println();
             System.out.println();
         }
+        if(userInput == 6){
+            addMovie();
+            System.out.println();
+            System.out.println();
+        }
         userOptions();
         userChoice();
     }// userChoice
 
+    public static Movie[] moviesArray = findAll().clone();
+
     public static String allMovies(){
         String allMovies = "";
-        for(Movie movie: findAll()){
+        for(Movie movie: moviesArray){
             allMovies = allMovies + "\n" + movie.getMovieName();
         }
         return allMovies;
@@ -67,7 +78,7 @@ public class MoviesApplication {
 
     public static String animatedMovies(){
         String animatedMovies = "";
-        for(Movie movie: findAll()){
+        for(Movie movie: moviesArray){
             if(movie.getMovieCategory().equalsIgnoreCase("animated")){
                 animatedMovies = animatedMovies + "\n" + movie.getMovieName();
             }
@@ -77,7 +88,7 @@ public class MoviesApplication {
 
     public static String dramaMovies(){
         String dramaMovies = "";
-        for(Movie movie: findAll()){
+        for(Movie movie: moviesArray){
             if(movie.getMovieCategory().equalsIgnoreCase("drama")){
                 dramaMovies = dramaMovies + "\n" + movie.getMovieName();
             }
@@ -87,7 +98,7 @@ public class MoviesApplication {
 
     public static String horrorMovies(){
         String horrorMovies = "";
-        for(Movie movie: findAll()){
+        for(Movie movie: moviesArray){
             if(movie.getMovieCategory().equalsIgnoreCase("horror")){
                 horrorMovies = horrorMovies + "\n" + movie.getMovieName();
             }
@@ -97,12 +108,20 @@ public class MoviesApplication {
 
     public static String scifiMovies(){
         String scifiMovies = "";
-        for(Movie movie: findAll()){
+        for(Movie movie: moviesArray){
             if(movie.getMovieCategory().equalsIgnoreCase("scifi")){
                 scifiMovies = scifiMovies + "\n" + movie.getMovieName();
             }
         }
         return scifiMovies;
+    }
+
+    public static void addMovie(){
+        Movie[] newMoviesArray = Arrays.copyOf(moviesArray, moviesArray.length + 1);
+        String name = input.getString("Enter movie name:");
+        String category = input.getString("Enter movie category:");
+        newMoviesArray[newMoviesArray.length - 1] = new Movie(name, category);
+        moviesArray = newMoviesArray;
     }
 
 }// class
